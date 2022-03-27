33f77774934b91621776de98780effb5c01a6c44
moralessf <fcesar_moan@hotmail.com>
2022-03-26 22:30:37 -0600

    Creación de archivos de JS para newPost.html

---
+++ js/newPost.js
@@ -0,0 +1,48 @@
+const urlDB = "https://devto-3e84b-default-rtdb.firebaseio.com/"
+
+const postImg=document.querySelector('#postImg');
+const postTitle=document.querySelector('#postTitle');
+const postTextContent=document.querySelector('#postTextContent');
+const postTags=document.querySelector('#postTags');
+
+const postear = (e) =>
+{
+    e.preventDefault();
+    /*let tags= ["#javascritp", "#html", "#web"];
+    createPost( "https://talently.tech/blog/wp-content/uploads/2021/11/Frame-57.png", "programacion web", "https://talently.tech/blog/wp-content/uploads/2021/11/Frame-57.png", tags, "Aqui va toda la descripción",  (body) => {
+    console.log(body)
+    })*/
+
+    console.log(postImg.value, postTitle.value, postImg.value,postTags.value,postTextContent.value);
+    // createPost(postImg.value,postTitle.value,postImg.value,postTags.value,postTextContent.value, (body)=>{
+    //     console.log(body);
+    // });
+}
+
+const createPost = ( image, title, avatar, tags, contentText, funcion) =>
+{
+    const url=`${urlDB}/posts.json`;
+    const tiempoActual = new Date();
+
+
+    let postID = `${tiempoActual.getTime()}${tiempoActual.getMilliseconds()}`;
+    let datetime = tiempoActual.getTime();
+    let counterReactions = 0;
+    let counterComents = 0;
+    let nameP = "Susana Perez";
+    
+    
+
+    const post = {postID, datetime, counterReactions, counterComents, image, title, avatar, tags, contentText, nameP};
+
+    fetch(url,{
+        method:'POST',
+        body:JSON.stringify(post),
+        headers:
+        {
+            'Content-Type':'application/json'
+        }
+    }).then((respuesta)=>respuesta.json())
+    .then((body)=>funcion(body))
+    .catch((error)=>console.log(error));
+}
