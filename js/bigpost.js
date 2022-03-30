7833d9c7a28b11f68ab2c45dbcc9fb81475141d0
Hiram Ramirez <hiram.j.ramirez@gmail.com>
2022-03-29 22:48:36 -0600

    update

---
+++ js/bigpost.js
@@ -0,0 +1,47 @@
+window.queryParams=new URLSearchParams(window.location.search);
+const id = queryParams.get('id');
+const avatar = document.querySelector('.avatar')
+const heroImg = document.querySelector('.hero-img')
+const userName = document.querySelector('.userName')
+const date = document.querySelector('.date')
+const title = document.querySelector('.title')
+const postContent = document.querySelector('.postContent')
+const tag = document.querySelector('.tag')
+
+const urlDB2 = "https://devto-3e84b-default-rtdb.firebaseio.com/"
+
+const callPost = async()=>
+{
+    try 
+    {
+        let url=`${urlDB2}posts/${id}.json`;
+        let respuesta = await fetch(url);
+        let post=await respuesta.json();
+        //console.log(post);
+        heroImg.src = post.image
+        avatar.src = post.image;
+        userName.textContent = post.nameP
+        date.textContent = post.dateTime
+        title.textContent = post.title
+        postContent.textContent = post.contentText
+
+        post.tags.forEach((x) => {
+            console.log(x)
+            tag.insertAdjacentHTML('beforeend',x + '&nbsp&nbsp' );
+
+        })
+        /* if(post.tags.length>0)
+        {
+            for(let i of post.tags)
+            {
+                tagCreator.value=i;
+                recoverEachTag(tagCreator);
+            }
+        } */
+    } catch (error)
+    {
+            
+    }
+}
+
+callPost()
