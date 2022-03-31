6933ce37a22efc4271107b29549cc043d51a9712
Martha Luna <lunam91@hotmail.com>
2022-03-30 19:39:32 -0600

    mejorando reusabilidad de codigo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,47 @@
 const urlDB = "https://devto-3e84b-default-rtdb.firebaseio.com/"
 const containerPosts = document.querySelector('#posts');
 
+const constructPOST = (post) =>{
+
+  let postHTML = `
+  <div ondblclick="openPost('${post.firebaseID}')" class="card mb-2">
+  `
+  if(post.image)
+ {postHTML += `<img class="card-img-top" src = "${post.image}">`} 
+   
+ postHTML +=  `<div class="d-flex">
+    
+      <img src="${post.avatar}" width="32em"
+        height="32em" class="rounded-pill ms-3 mt-3 gap-3" alt="posts_avatar">
+      <div class="d-flex flex-column mt-3 ms-1"><span>${post.nameP}</span>
+        <small>${post.day}/${post.month}/${post.year}</small>
+      </div>
+    </div>
+    <div class="card-body">
+      <h4 class="card-title">${post.title}</h4>`
+      if(post.tags)
+      post.tags.forEach((tag)=>{
+        postHTML+=`${tag}    `
+      })
+      
+      if(post.contentText)
+      postHTML+=`<p class="card-text">${post.contentText.substring(0,50)}...</p>`
+      postHTML+=`<div class="d-flex justify-content-between">
+        <div><span><i class="bi bi-suit-heart"></i> ${post.counterReactions} Reactios</span>
+          <span><i class="bi bi-chat-right"></i> ${post.counterComents} Comments</span>
+        </div>
+        <div>
+        <a onclick="editPost('${post.firebaseID}')" class="btn btn-secondary btn-sm">Update</a>
+        <a onclick="deletePost('${post.firebaseID}')" class="btn btn-secondary btn-sm">Delete</a>
+        </div>
+      </div>
+    </div>
+  </div>`
+
+  return postHTML;
+
+}
+
 const getPosts2 = async () => {
   const url = `${urlDB}/posts.json`;
   const respuesta = await fetch(url);
@@ -28,42 +69,8 @@ const getPosts2 = async () => {
   });
 
   postValues.forEach((post)=> {
-    //console.log(post.datetime);
-    let postHTML = `
-       <div ondblclick="openPost('${post.firebaseID}')" class="card mb-2">
-       `
-       if(post.image)
-      {postHTML += `<img class="card-img-top" src = "${post.image}">`} 
-        
-      postHTML +=  `<div class="d-flex">
-          <img src="${post.avatar}" width="32em"
-            height="32em" class="rounded-pill ms-3 mt-3 gap-3" alt="posts_avatar">
-          <div class="d-flex flex-column mt-3 ms-1"><span>${post.nameP}</span>
-            <small>${post.day}/${post.month}/${post.year}</small>
-          </div>
-        </div>
-        <div class="card-body">
-          <h4 class="card-title">${post.title}</h4>`
-    if (post.tags)
-      post.tags.forEach((tag) => {
-        postHTML += `${tag}    `
-      })
 
-    if (post.contentText)
-      postHTML += `<p class="card-text">${post.contentText.substring(0,50)}...</p>`
-    postHTML += `<div class="d-flex justify-content-between">
-            <div><span><i class="bi bi-suit-heart"></i> ${post.counterReactions} <span class="bignav">Reactios</span></span>
-              <span><i class="bi bi-chat-right"></i> ${post.counterComents} <span class="bignav">Comments</span></span>
-            </div>
-            <div>
-              <a onclick="editPost('${post.firebaseID}')" class="btn btn-secondary btn-sm">Update</a>
-              <a onclick="deletePost('${post.firebaseID}')" class="btn btn-secondary btn-sm">Delete</a>
-            </div>
-          </div>
-        </div>
-      </div>`
-
-    containerPosts.insertAdjacentHTML('afterbegin', postHTML);
+    containerPosts.insertAdjacentHTML('afterbegin', constructPOST(post));
   });  
 }
 
--- js/search.js
+++ js/search.js
@@ -1,7 +1,6 @@
 
 
 const find=document.querySelector('#search');
-
 const search=async()=>
 {
     console.log("entre")
@@ -37,43 +36,8 @@ const search=async()=>
     const title = find.value.toLowerCase();
 
     const posts = postValues.filter((post)=>post.title.toLowerCase().includes(title));
-    posts.forEach((post)=>{
-
-      let postHTML = `
-      <div ondblclick="openPost('${post.firebaseID}')" class="card mb-2">
-      `
-      if(post.image)
-     {postHTML += `<img class="card-img-top" src = "${post.image}">`} 
-       
-     postHTML +=  `<div class="d-flex">
-        
-          <img src="${post.avatar}" width="32em"
-            height="32em" class="rounded-pill ms-3 mt-3 gap-3" alt="posts_avatar">
-          <div class="d-flex flex-column mt-3 ms-1"><span>${post.nameP}</span>
-            <small>${post.day}/${post.month}/${post.year}</small>
-          </div>
-        </div>
-        <div class="card-body">
-          <h4 class="card-title">${post.title}</h4>`
-          if(post.tags)
-          post.tags.forEach((tag)=>{
-            postHTML+=`${tag}    `
-          })
-          
-          if(post.contentText)
-          postHTML+=`<p class="card-text">${post.contentText.substring(0,50)}...</p>`
-          postHTML+=`<div class="d-flex justify-content-between">
-            <div><span><i class="bi bi-suit-heart"></i> ${post.counterReactions} Reactios</span>
-              <span><i class="bi bi-chat-right"></i> ${post.counterComents} Comments</span>
-            </div>
-            <div>
-            <a onclick="editPost('${post.firebaseID}')" class="btn btn-secondary btn-sm">Update</a>
-            <a onclick="deletePost('${post.firebaseID}')" class="btn btn-secondary btn-sm">Delete</a>
-            </div>
-          </div>
-        </div>
-      </div>`
-
-      containerPosts.insertAdjacentHTML('afterbegin',postHTML);
+    posts.forEach((post)=>{ 
+      containerPosts.insertAdjacentHTML('afterbegin', constructPOST(post));
     })
 }
+
